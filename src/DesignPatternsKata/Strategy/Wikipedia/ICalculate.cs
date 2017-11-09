deb65c37b52f0c58869d8b368b2c0d438e9a1d67
Jannes Coetzee <jannesrsa@gmail.com>
2017-11-09 17:11:58 +0200

    STRATEGY PATTERN Wikipedi calculate client.

---
+++ src/DesignPatternsKata/Strategy/Wikipedia/ICalculate.cs
@@ -0,0 +1,8 @@
+﻿namespace DesignPatternsKata.Strategy.Wikipedia
+{
+    //The interface for the strategies
+    public interface ICalculate
+    {
+        int Calculate(int value1, int value2);
+    }
+}
---
+++ src/DesignPatternsKataTests/Strategy/Wikipedia/CalculateClientTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesignPatternsKata.Strategy.Wikipedia.Tests
+{
+    [TestClass()]
+    public class CalculateClientTests
+    {
+        [TestMethod()]
+        public void CalculateTest_Minus()
+        {
+            // Arrange
+            var calculateClient = new CalculateClient(new Minus());
+
+            // Action
+            var actual = calculateClient.Calculate(6, 1);
+
+            // Assert
+            Assert.AreEqual(5, actual);
+        }
+
+        [TestMethod()]
+        public void CalculateTest_Plus()
+        {
+            // Arrange
+            var calculateClient = new CalculateClient(new Plus());
+
+            // Action
+            var actual = calculateClient.Calculate(4, 3);
+
+            // Assert
+            Assert.AreEqual(7, actual);
+        }
+
+        [TestMethod()]
+        public void CalculateTest_Minus_Then_Plus()
+        {
+            // Arrange
+            var calculateClient = new CalculateClient(new Minus());
+            calculateClient.Strategy = new Plus();
+
+            // Action
+            var actual = calculateClient.Calculate(4, 3);
+
+            // Assert
+            Assert.AreEqual(7, actual);
+        }
+    }
+}
