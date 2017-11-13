453fe5874df027d676cabdf6e4ccb4387fc3e01b
Jannes Coetzee <jannesrsa@gmail.com>
2017-11-13 17:33:22 +0200

    RegionNotifyPropertyChanged

---
+++ src/DesignPatternsKata/Observer/RegionNotifyPropertyChanged.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DesignPatternsKata.Observer
+{
+    public class RegionNotifyPropertyChanged : INotifyPropertyChanged
+    {
+        private string _name;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value != this._name)
+                {
+                    this._name = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        // This method is called by the Set accessor of each property.
+        // The CallerMemberName attribute that is applied to the optional propertyName
+        // parameter causes the property name of the caller to be substituted as an argument.
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ src/DesignPatternsKataTests/Observer/RegionNotifyPropertyChangedTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.ComponentModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesignPatternsKata.Observer.Tests
+{
+    [TestClass()]
+    public class RegionNotifyPropertyChangedTests
+    {
+        [TestMethod()]
+        public void SetNameTest()
+        {
+            // Arrange
+            var region = new RegionNotifyPropertyChanged
+            {
+                Name = Guid.NewGuid().ToString()
+            };
+
+            var expected = Guid.NewGuid().ToString();
+
+            var propertyName = default(string);
+            PropertyChangedEventHandler handler = (s, e) =>
+            {
+                propertyName = e.PropertyName;
+            };
+
+            region.PropertyChanged += handler;
+
+            // Action
+            region.Name = expected;
+
+            // Assert
+            Assert.AreEqual(expected, region.Name);
+            Assert.AreEqual(nameof(region.Name), propertyName);
+        }
+
+        [TestMethod()]
+        public void SetNameTest_MultipleHandlers()
+        {
+            // Arrange
+            var region = new RegionNotifyPropertyChanged
+            {
+                Name = Guid.NewGuid().ToString()
+            };
+
+            var expected = Guid.NewGuid().ToString();
+
+            var propertyName = default(string);
+            PropertyChangedEventHandler handler = (s, e) =>
+            {
+                propertyName = e.PropertyName;
+            };
+
+            region.PropertyChanged += handler;
+
+            var prePend = Guid.NewGuid().ToString();
+            var propertyName2 = default(string);
+
+            PropertyChangedEventHandler handler2 = (s, e) =>
+            {
+                propertyName2 = prePend + e.PropertyName;
+            };
+
+            region.PropertyChanged += handler2;
+
+            // Action
+            region.Name = expected;
+
+            // Assert
+            Assert.AreEqual(expected, region.Name);
+            Assert.AreEqual(nameof(region.Name), propertyName);
+            Assert.AreEqual(prePend + nameof(region.Name), propertyName2);
+        }
+    }
+}
