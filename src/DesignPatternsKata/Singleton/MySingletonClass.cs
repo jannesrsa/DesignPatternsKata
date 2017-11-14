3429ec2d5be15ffd62eaca7c28d859232f7710b5
Jannes Coetzee <jannesrsa@gmail.com>
2017-11-14 09:00:24 +0200

    SINGLETON PATTERN

---
+++ src/DesignPatternsKata/Singleton/MySingletonClass.cs
@@ -0,0 +1,13 @@
+﻿namespace DesignPatternsKata.Singleton
+{
+    public class MySingletonClass
+    {
+        public static MySingletonClass Instance { get; } = new MySingletonClass();
+
+        public int SomeValue { get; set; }
+
+        private MySingletonClass()
+        {
+        }
+    }
+}
---
+++ src/DesignPatternsKataTests/Singleton/MySingletonClassTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesignPatternsKata.Singleton.Tests
+{
+    [TestClass()]
+    public class MySingletonClassTests
+    {
+        [TestMethod()]
+        public void MySingletonClass_SomeValue()
+        {
+            // Arrange
+            var first = MySingletonClass.Instance;
+            var second = MySingletonClass.Instance;
+
+            // Action
+            first.SomeValue++;
+
+            // Assert
+            Assert.AreSame(first, second);
+            Assert.AreEqual(first.SomeValue, second.SomeValue);
+
+            // Action
+            second.SomeValue++;
+
+            // Assert
+            Assert.AreEqual(first.SomeValue, second.SomeValue);
+        }
+
+        [TestMethod()]
+        public void MySingletonClass_SomeValue_Threading()
+        {
+            var taskList = new List<Task>();
+            for (int i = 0; i < 100; i++)
+            {
+                taskList.Add(Task.Run(() => MySingletonClass_SomeValue()));
+            }
+
+            Task.WaitAll(taskList.ToArray());
+        }
+    }
+}
